33eb27ea7fef092d078689c0189534cb3b03f373
Bhanu Prasanna <63473951+bhanuprasanna2001@users.noreply.github.com>
2024-02-19 11:37:42 +0100

    Update

--- src/component/front.js
+++ src/component/front.js
@@ -96,12 +96,14 @@ function Front() {
                                value={emailID} onChange={(e) => setEmailID(e.target.value)}
                                className="mt-1 px-4 py-2 block w-full rounded-md bg-gray-200"
                                placeholder="name@email.com"/>
+                        <h2 className="text-base text-center text-[#888888] sm:text-sm">We'll never share your info with anyone.</h2>
                     </div>
                     <div className="text-center">
-                        <button type="submit" className="px-4 py-2 bg-zinc-700 text-white rounded-xl" onClick={openModal}>
+                    <button type="submit" className="px-4 py-2 bg-zinc-700 text-white rounded-xl"
+                                onClick={openModal}>
                             Join the Waitlist!
                         </button>
-                        <Modal isOpen={isOpen} onClose={closeModal} />
+                        <Modal isOpen={isOpen} onClose={closeModal}/>
                     </div>
                 </form>
             </div>
